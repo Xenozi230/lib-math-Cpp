f9781a0a221d49e82058b41583134790a57a7269
Xenozi230 <enzotel08@gmail.com>
2024-11-21 19:17:32 +0100

    Implement basic math library functions

---
+++ src/mathlib.cpp
@@ -0,0 +1,19 @@
+#include "mathlib.h"
+#include <cmath>
+#include <stdexcept>
+
+int add (int a, int b){
+    return a + b;
+}
+int sub(int a, int b){
+    return a - b;
+}
+int mul(int a, int b){
+    return a * b;
+}
+double div(int a, int b){
+    if(b == 0){
+        throw std::invalid_argument("Division by zero");
+    }
+    return static_cast<double>(a) / b;
+}
---
+++ src/mathlib.h
@@ -0,0 +1,13 @@
+#ifndef MATHLIB_H
+#define MATHLIB_H
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+double div(int a, int b);
+
+
+
+
+
+#endif
